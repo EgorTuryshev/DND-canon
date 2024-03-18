dc2ce20f608c84ae2e5985b90f35fcedf2a82a1a
EgorTuryshev <egor.turyshev@mail.ru>
2024-03-18 18:42:51 +0300

    randomizer is working

--- script.lua
+++ script.lua
@@ -2,12 +2,7 @@ dofile("scripts/forts.lua")
 
 local RPdebug = true
 
-local weaponfired
 function OnWeaponFired(teamId, saveName, weaponId, projectileNodeId, projectileNodeIdFrom)
-    if not weaponfired then
-        Log("fired weapon")
-        weaponfired = true
-    end
     if weaponId ~= -1 then
         ProtectedFunction(OnWeaponFiredpcall,teamId, saveName, weaponId, projectileNodeId, projectileNodeIdFrom)
     end
@@ -15,56 +10,52 @@ end
 
 function OnWeaponFiredpcall(teamId, saveName, weaponId, projectileNodeId, projectileNodeIdFrom)
 
+    local name = GetNodeProjectileSaveName(projectileNodeId)
     local velocity = NodeVelocity(projectileNodeId)
     local pos = NodePosition(projectileNodeId)
     local age = GetNodeProjectileTimeRemaining(projectileNodeId)
+    local agetrigger = GetNodeProjectileAgeTrigger(projectileNodeId)
 
-    local projectile = SpawnRandomProjectile(projectileNodeId, weaponId, teamId, pos, velocity, age)
-    LogDebug("exception mark3")
+    if data.AgeTriggers[projectileNodeIdFrom] then
+        SetNodeProjectileAgeTrigger(projectileNodeId,data.AgeTriggers[projectileNodeIdFrom])
+    end
+    if name:find("nocol") or GetNodeProjectileSaveName(projectileNodeIdFrom):find("nocol") then return end
+
+    local projectile = SpawnRandomProjectile(projectileNodeId, weaponId, teamId, pos, velocity, age, agetrigger)
     if projectile ~= "invalid" then
         DestroyProjectile(projectileNodeId)
     end
 end
 
-function SpawnRandomProjectile(origProjectileId, origWeaponId, teamId, pos, velocity, age)
+function SpawnRandomProjectile(origProjectileId, origWeaponId, teamId, pos, velocity, age, agetrigger)
     local selectedShell = GetRandomIntegerLocal(1, 2)
-    local proj = "machinegun"
-    --if selectedShell == 2 then
-    --    proj = "shell20"
-    --end
-  
-    local origname = GetNodeProjectileSaveName(origProjectileId)
-    local origgravity,projgravity,speedmultiplier,inverser = math.max(math.abs(GetProjectileTypeGravity(origname,teamId)),50),math.abs(GetProjectileTypeGravity(proj,teamId))
-
-    if origgravity ~= projgravity and projgravity > 10 and GetProjectileParamString(proj,teamId,"ProjectileType","mortar") ~= PROJECTILE_TYPE_MISSILE then
-        if origgravity > projgravity then
-            inverser = origgravity/projgravity
-            speedmultiplier = projgravity/origgravity
-            local formula = 1/inverser^0.75
-            LogDebug("vel, origgrav, projgrav, form, 1-form: " .. velocity.." "..origgravity.." "..projgravity.." "..(formula).." "..(1-formula))
-            velocity = Vec3(velocity.x*((formula)+speedmultiplier*(1-formula)),velocity.y*((formula)+speedmultiplier*(1-formula)))
-            LogDebug("velocity, speedmult: " .. velocity.." "..speedmultiplier)
-        else
-            inverser = projgravity/origgravity
-            speedmultiplier = origgravity/projgravity
-            local formula = 1/inverser^1.25
-            LogDebug("vel, origgrav, projgrav, form, 1-form: " .. velocity.." "..origgravity.." "..projgravity.." "..(formula).." "..(1-formula))
-            velocity = Vec3(velocity.x*((1-formula)+speedmultiplier/(formula)),velocity.y*((1-formula)+speedmultiplier/(formula)))
-            LogDebug("velocity, speedmult: " .. velocity.." "..speedmultiplier)
-        end
+    local proj = "shell1"
+    if selectedShell == 2 then
+        proj = "shell20"
     end
-
-    LogDebug("exception mark1")
-
-    local projectileId = dlc2_CreateProjectile(proj, proj, teamId, pos, velocity, age)
-    LogDebug("exception mark2")
-
+    local projectileId = dlc2_CreateProjectile(proj.."_nocol", proj, teamId, pos, velocity, age)
     if origWeaponId > 0 then
         NoColProjectileEnd(projectileId,0)
     else
         SetNodeProjectileAgeTrigger(projectileId,0.1)
     end
+    if agetrigger and agetrigger > 0 then
+        data.AgeTriggers[projectileId] = agetrigger
+    end
     return projectileId
+end
+
+function NoColProjectileEnd(id,timesrepeated)
+    if not NodeExists(id) then return end
+    timesrepeated = timesrepeated+1
+    local pos,vel = NodePosition(id),NodeVelocity(id)
+    local ray = CastRay(pos,pos+Vec3(vel.x/100,vel.y/100),0,0)
+    if ray == RAY_HIT_DEVICE then
+        ScheduleCall(0,NoColProjectileEnd,id,timesrepeated)
+        if data.AgeTriggers[id] then
+            data.AgeTriggers[id] = data.AgeTriggers[id]-0.04*timesrepeated+0.02
+        end
+    else SetNodeProjectileAgeTrigger(id,0.04*timesrepeated) end
 end
 
 function ProtectedFunction(func,...)
@@ -82,19 +73,10 @@ end
 
 function LogDebug(s) if RPdebug then Log(s) end end
 
-function NoColProjectileEnd(id,timesrepeated)
-    if not NodeExists(id) then return end
-    timesrepeated = timesrepeated+1
-    local pos,vel = NodePosition(id),NodeVelocity(id)
-    local ray = CastRay(pos,pos+Vec3(vel.x/100,vel.y/100),0,0)
-    if ray == RAY_HIT_DEVICE then ScheduleCall(0,NoColProjectileEnd,id,timesrepeated)
-    else SetNodeProjectileAgeTrigger(id,0.04*timesrepeated) end
-end
-
 function Load()
     ProtectedFunction(Loadpcall)
 end
 
 function Loadpcall()
-
+    if not data.AgeTriggers then data.AgeTriggers = {} end
 end
--- weapons/projectile_list.lua
+++ weapons/projectile_list.lua
@@ -4,22 +4,48 @@ if howitzer then
     shell1 = DeepCopy(howitzer)
     shell1.SaveName = "shell1"
     shell1.ProjectileDamage = 750
-	shell1.AntiAirHitpoints = 110
+    shell1.AntiAirHitpoints = 110
     shell1.Impact = 500000
     shell1.ProjectileSprite = path.. "/weapons/sprites/shell1.png"
-	shell1.ProjectileThickness = 10.0
+    shell1.ProjectileThickness = 10.0
     shell1.ProjectileShootDownRadius = 60
-end
-table.insert(Projectiles, shell1)
+    shell1.DndProjectile = true
+    Projectiles[#Projectiles+1] = shell1
 
-if howitzer then
     shell20 = DeepCopy(howitzer)
     shell20.SaveName = "shell20"
     shell20.ProjectileDamage = 750
-	shell20.AntiAirHitpoints = 110
+    shell20.AntiAirHitpoints = 110
     shell20.Impact = 500000
     shell20.ProjectileSprite = path.. "/weapons/sprites/shell20.png"
-	shell20.ProjectileThickness = 10.0
+    shell20.ProjectileThickness = 10.0
     shell20.ProjectileShootDownRadius = 60
+    shell20.DndProjectile = true
+    Projectiles[#Projectiles+1] = shell20
 end
-table.insert(Projectiles, shell20)
+
+
+for i, v in ipairs(Projectiles) do
+    if v.DndProjectile then
+       local nocol = DeepCopy(v)
+       nocol.SaveName = v.SaveName .. "_nocol"
+       nocol.CollidesWithProjectiles = false
+       nocol.ProjectileIncendiary = false
+       nocol.AlwaysPassDevices = true
+       nocol.WeaponDamageBonus = 0
+       nocol.EMPDuration = 0
+       nocol.ProjectileDamage = 0
+       nocol.DndProjectile = false
+       if not nocol.DamageMultiplier then nocol.DamageMultiplier = {} end
+       if not nocol.Effects then nocol.Effects = {} end
+       if not nocol.Effects.Impact then nocol.Effects.Impact = {} end
+       nocol.DamageMultiplier[#nocol.DamageMultiplier + 1] = { SaveName = "weapon", Direct = 0 }
+       nocol.Effects.Impact.device = {
+           Effect = nil,
+           Terminate = false,
+           Splash = false
+       }
+       nocol.Effects.Age = { ["t500"] = { Projectile = { Count = 1, Type = v.SaveName }, Splash = false } }
+       Projectiles[#Projectiles+1] = nocol
+   end
+end    
